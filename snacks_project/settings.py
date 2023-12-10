83196d2dc0057aa30de08df427179fc21e83f44b
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-12-10 01:10:06 -0600

    finished setting up tailwind flowbite

--- snacks_project/settings.py
+++ snacks_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "snacks"
+    "snacks",
+    "compressor"
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# compressor settings taken from the class repo
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
